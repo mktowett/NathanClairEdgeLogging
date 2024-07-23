b95a425f5d3c2e1f6f29838007213aae52d9e70d
mktowett <marvintowett@gmail.com>
2024-07-23 17:13:49 +0300

    added code documentation in file

--- edgencg/src/main/java/com/mk/jetpack/edgencg/EdgeNCG.kt
+++ edgencg/src/main/java/com/mk/jetpack/edgencg/EdgeNCG.kt
@@ -1,4 +1,39 @@
 package com.mk.jetpack.edgencg
 
-class EdgeNCG {
+import com.mk.jetpack.edgencg.logging.EdgeLogCollector
+import com.mk.jetpack.edgencg.logging.EdgeLogger
+import timber.log.Timber
+import javax.inject.Inject
+
+/**
+ * EdgeNCG is responsible for initializing logging mechanisms and collecting device data logs.
+ * It uses dependency injection to obtain instances of EdgeLogCollector and EdgeLogger for its operations.
+ *
+ * @property edgeLogCollector An instance of EdgeLogCollector used to collect device data logs.
+ * @property edgeLogger An instance of EdgeLogger used for logging the collected device data.
+ */
+class EdgeNCG @Inject constructor(
+    private val edgeLogCollector: EdgeLogCollector,
+    private val edgeLogger: EdgeLogger
+) {
+
+
+    /**
+     * Initializes the logging framework and collects device data logs.
+     * If the application is in debug mode, it plants a Timber DebugTree to enable detailed logging.
+     * It then collects device data logs and logs them using the EdgeLogger instance.
+     *
+     * @param isDebug Indicates whether the application is running in debug mode.
+     */
+    fun init(isDebug: Boolean) {
+        // Initialize Timber
+        if (isDebug) {
+            Timber.plant(Timber.DebugTree())
+        }
+
+        // Collect device data
+        val deviceData = edgeLogCollector.collectLogs()
+        edgeLogger.d("Device Data: $deviceData")
+
+    }
 }
--- edgencg/src/main/java/com/mk/jetpack/edgencg/logging/EdgeLogCollector.kt
+++ edgencg/src/main/java/com/mk/jetpack/edgencg/logging/EdgeLogCollector.kt
@@ -3,8 +3,20 @@ package com.mk.jetpack.edgencg.logging
 import com.mk.jetpack.edgencg.data.DeviceDataCollector
 import javax.inject.Inject
 
+
+/**
+ * EdgeLogCollector is responsible for collecting logs related to device data.
+ * It utilizes the DeviceDataCollector to gather necessary device information.
+ *
+ * @property deviceDataCollector An instance of DeviceDataCollector used to collect device-specific data.
+ */
 class EdgeLogCollector @Inject constructor(private val deviceDataCollector: DeviceDataCollector) {
 
+    /**
+     * Collects logs from the device.
+     *
+     * @return A map containing key-value pairs of device data.
+     */
     fun collectLogs(): Map<String, String> {
         return deviceDataCollector.collectDeviceData()
     }
